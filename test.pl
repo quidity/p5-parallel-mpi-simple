88973a1551f0b0f1bdecbe6d99473b5bf240b175
Alex Gough <alex@earth.li>
2011-06-06 04:45:28 +0100

    automatically detect mpicc and use it

--- test.pl
+++ test.pl
@@ -3,5 +3,8 @@ $incs .= " -I$_" foreach @INC;
 my @newout = sort {
     (($a =~ /(\d+)/g)[0] <=> ($b =~ /(\d+)/g)[0])
 } `mpirun -np 2 $^X $incs ic.pl`;
-print "1..".scalar(@newout)."\n";
+print "1..24";
+if (@newout < 24) {
+    print "not ok 1 # mpirun failed.  Do you need to start mpd?\n";
+}
 print @newout;
