46512d03d40eef82563d1b0d13173eaaeb363ddd
Alex Gough <alex@earth.li>
2011-06-06 05:10:17 +0100

    Fix missing newline in test output

--- test.pl
+++ test.pl
@@ -3,7 +3,7 @@ $incs .= " -I$_" foreach @INC;
 my @newout = sort {
     (($a =~ /(\d+)/g)[0] <=> ($b =~ /(\d+)/g)[0])
 } `mpirun -np 2 $^X $incs ic.pl`;
-print "1..24";
+print "1..24\n";
 if (@newout < 24) {
     print "not ok 1 # mpirun failed.  Do you need to start mpd?\n";
 }
